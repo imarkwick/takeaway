17e7f1d202d226ca392534501cb2cf3b67218278
imarkwick <izzymarkwick@gmail.com>
2014-11-10 00:05:52 +0000

    Adding a menu items hash

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,5 +1,27 @@
 class Menu 
 
+	attr_reader :dish, :price
 
+	def initialize
+		@menu = {:dish => :price}
+	end
 
+	def hungry_for
+		@takeout_menu = {
+			"Burrito" => 5.00,
+			"Taccos" => 7.00,
+			"Salsa" => 2.00,
+			"Guacamole" => 2.00,
+			"Fajitas" => 8.00
+		}
+	end
+
+	def display_menu
+		hungry_for.each { |dish, price| puts "#{dish} £#{price}" }
+	end
+
+	def total_dishes
+		hungry_for.count
+	end
 end
+
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,5 +2,16 @@ require 'menu'
 
 describe Menu do  
 
-	
+	let(:menu) {Menu.new}
+
+	it 'should have a selection of dishes' do
+		expect(menu.hungry_for["Taccos"]).to eq 7.00
+	end
+
+	it 'should know how many dishes there are on the menu' do
+		expect(menu.total_dishes).to eq 5
+	end
+
+	# it should be able to have dishes added
+
 end
